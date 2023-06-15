90f55b8e8b1386187e50d8319bb8f28c44236d65
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-15 13:17:26 +0330

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("xxxxxxxxxx");
+            }
+        }
+    }
+}
